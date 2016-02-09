31796f132429e34aa6353f16b1360936b59ef162
Paige <paige.fenwick@att.net>
2016-02-09 18:08:14 -0500

    5.4

--- app.js
+++ app.js
@@ -3,20 +3,14 @@
 
 
 
-  app.controller('StoreController', function() {
-    this.products = gems;
+  app.controller('StoreController', ['$http', function($http){
+  var store = this;
+  store.products = [];
+
+  $http.get('/store-products.json').success(function(data){
+    store.products = data;
   });
-
-  app.controller("ReviewController", function(){
-
-    this.review = {};
-
-    this.addReview = function(product){
-      product.reviews.push(this.review);
-      this.review = {};
-    };
-
-  });
+}]);
 
   
   var gems = [
